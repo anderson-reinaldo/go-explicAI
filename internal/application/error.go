c3b544684ff1b7cc291a1a1453f029e5d3096ab1
REINALDO <andersonnreinaldo@gmail.com>
2026-09-24 23:25:36 -0300

    finalizando test suite

--- internal/application/error.go
+++ internal/application/error.go
@@ -3,7 +3,8 @@ package application
 import "errors"
 
 var (
-	MissingFile    = errors.New("missing file to upload")
-	InvalidFile    = errors.New("invalid file")
-	FailedReadFile = errors.New("fail read to upload")
+	MissingFile     = errors.New("missing file to upload")
+	InvalidFile     = errors.New("invalid file")
+	FailedReadFile  = errors.New("fail read to upload")
+	SummaryNotFound = errors.New("summary not found")
 )
